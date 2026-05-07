3a23791399cbf89dedafd430a74364866efb235a
MarianoEs <marianoesser@gmail.com>
2026-05-06 23:52:52 -0300

    Agregar archivos de proyecto.

---
+++ poo-2026/Ejemplo1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poo_2026
+{
+    public class Ejemplo1
+    {
+        CamposyPropiedades ej1 = new CamposyPropiedades();
+        
+        public Ejemplo1()
+        {
+                ej1.Nombre = "Pepe";
+            Console.WriteLine(ej1.Nombre);
+        }
+    }
+
+    public class CamposyPropiedades
+    {
+        private string nombre;
+
+        public String Nombre { get; set; }
+
+    }
+}
---
+++ poo-2026/Ejemplo2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poo_2026
+{
+    public class Ejemplo2
+    {
+
+        //pasar un argumento por valor
+        private Tipos tt = new Tipos();
+        public Ejemplo2()
+        {
+            int x = 10;
+            Console.WriteLine($"Antes de llamar al metodo Valor {x}");
+
+            tt.Valor(x);
+
+            Console.WriteLine($"Despues de llamar al metodo Valor {x}");
+        }
+    }
+
+    public class Tipos
+    {
+        public void Valor(int x)
+        {
+            Console.WriteLine($"Al inicio del metodo Valor {x}");
+
+            x = x + 1000;
+
+            Console.WriteLine($"Al final del metodo Valor {x}");
+        }
+    }
+}
