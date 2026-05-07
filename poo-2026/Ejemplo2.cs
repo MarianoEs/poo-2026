b6b993f2be5d5b62309414e20857591a53da781d
MarianoEs <marianoesser@gmail.com>
2026-05-07 00:05:39 -0300

    Tipos

--- poo-2026/Ejemplo2.cs
+++ poo-2026/Ejemplo2.cs
@@ -11,12 +11,17 @@ namespace poo_2026
         private Tipos tt = new Tipos();
         public Ejemplo2()
         {
-            int x = 10;
-            Console.WriteLine($"Antes de llamar al metodo Valor {x}");
+            // int x = 10;
+            // Console.WriteLine($"Antes de llamar al metodo Valor {x}");
+            //tt.Valor(x);
+            //Console.WriteLine($"Despues de llamar al metodo Valor {x}");
 
-            tt.Valor(x);
-
-            Console.WriteLine($"Despues de llamar al metodo Valor {x}");
+            //pasar un argumento por referencia
+            Pepe p = new Pepe();
+            p.Numero = 5;
+            Console.WriteLine ($"Antes de llamar al metodo Referencia {p.Numero}");
+            tt.Referencia(p);
+            Console.WriteLine($"Despues de llamar al metodo Referencia {p.Numero}");
         }
     }
 
@@ -30,5 +35,17 @@ namespace poo_2026
 
             Console.WriteLine($"Al final del metodo Valor {x}");
         }
+
+        public void Referencia(Pepe p)
+        {
+            Console.WriteLine($"Al inicio del metodo Referencia {p.Numero}");
+            p.Numero = p.Numero + 1000;
+            Console.WriteLine($"Al final del metodo Referencia {p.Numero}");
+        }
+    }
+
+    public class Pepe 
+    {
+        public int Numero { get; set; }
     }
 }
