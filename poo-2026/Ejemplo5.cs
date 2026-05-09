19b4c229bbbed2a7962528b477ae1f1b7b285785
MarianoEs <marianoesser@gmail.com>
2026-05-09 00:46:00 -0300

    Interfase

---
+++ poo-2026/Ejemplo5.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace poo_2026
+{
+
+    public class Ejemplo5
+    {
+        //public Ejemplo5()  // Constructor
+       // {
+            //PersonaBD personaBD = new PersonaBD();
+            //personaBD.ObtenerPersona(1);
+
+            //Console.WriteLine("---------------------------------------------");
+
+            //PersonaLista pLista = new PersonaLista();
+            //pLista.ObtenerPersona(2);
+
+           // Console.WriteLine("---------------------------------------------");
+
+        //}
+            public void EjecutarBuscarPersona(IBuscarPersona bp, int id)
+            {
+                bp.ObtenerPersona(id);
+            }
+        public  IBuscarPersona EjecutarBuscarPersona(tipoBusqueda tipo)
+        {
+            switch (tipo)
+            {
+                case tipoBusqueda.BaseDatos:
+                    return new PersonaBD();
+                case tipoBusqueda.Lista:
+                    return new PersonaLista();
+                default:
+                    throw new ArgumentException("Tipo de busqueda no valido");
+            }
+        }
+
+    }
+
+    public class PersonaBD : IBuscarPersona
+    {
+        public void ObtenerPersona(int id)
+        {
+            // Simulamos la obtencion de una persona desde la base de datos
+            Console.WriteLine($"Obteniendo persona id = {id} de la base de datos...");
+        }
+    }
+
+    public class PersonaLista : IBuscarPersona
+    {
+        public void ObtenerPersona(int id)
+        {
+            // Simulamos la obtencion de una persona desde una lista
+            Console.WriteLine($"Obteniendo persona id = {id} desde una lista...");
+        }
+    }
+
+    //Interfase
+    public interface IBuscarPersona
+    {
+        void ObtenerPersona(int id);
+    }
+
+    //Enumeracion
+    public enum tipoBusqueda
+    {
+        BaseDatos,
+        Lista
+    }
+
+
+}
--- poo-2026/Program.cs
+++ poo-2026/Program.cs
@@ -1,9 +1,28 @@
 ﻿using poo_2026;
 
-//Ejemplo1 ej1 = new Ejemplo1();
+//Ejemplo1 ej = new Ejemplo1();
 
-//Ejemplo2 ej2 = new Ejemplo2();
+//Ejemplo2 ej = new Ejemplo2();
 
-//Ejemplo3 ej3 = new Ejemplo3();
+//Ejemplo3 ej = new Ejemplo3();
 
-Ejemplo4 ej4 = new Ejemplo4();
+//Ejemplo4 ej = new Ejemplo4();
+
+Ejemplo5 ej = new Ejemplo5();
+
+
+//ej.EjecutarBuscarPersona(new PersonaBD(), 1);
+//Console.WriteLine("---------------------------------------------");
+
+//ej.EjecutarBuscarPersona(new PersonaLista(), 2);
+//Console.WriteLine("---------------------------------------------");
+
+
+IBuscarPersona buscadorBD = ej.EjecutarBuscarPersona(tipoBusqueda.BaseDatos);
+buscadorBD.ObtenerPersona(1);
+Console.WriteLine("---------------------------------------------");
+
+var buscadorLista = ej.EjecutarBuscarPersona(tipoBusqueda.Lista);
+buscadorLista.ObtenerPersona(1);
+Console.WriteLine("---------------------------------------------");
+
